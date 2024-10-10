76633cec1adbed2872afb5bf6ea06cb26b5a8f68
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2024-10-10 18:25:02 -0400

    ff: fix mobile view for auctions (#185)

--- src/app/auctions/components/LiveAuctionsHero.tsx
+++ src/app/auctions/components/LiveAuctionsHero.tsx
@@ -5,29 +5,48 @@ import { HiMiniArrowRight } from "react-icons/hi2";
 
 export function LiveAuctionsHero() {
   return (
-    <ValenceProductBrand
-      img={
-        <Image
-          className="max-h-24 w-auto"
-          src="/img/auctions.svg"
-          alt="Auctions illustration"
-          width={134}
-          height={126}
-        />
-      }
-    >
-      <h1 className="text-2xl font-bold">Auctions (beta)</h1>
-      <p className="max-w-lg text-pretty pt-1">
-        Dutch auctions to enable trades for accounts using the{" "}
-        <Link
-          className="font-semibold hover:border-b-[1.6px] hover:border-valence-black"
-          href="/rebalancer"
-        >
-          Rebalancer
-        </Link>
-        . Auctions begin at 12:00 UTC daily, and close after 24 hours.
-      </p>
-      <div className="flex flex-row gap-4 pt-2">
+    <>
+      <ValenceProductBrand
+        img={
+          <Image
+            className="max-h-24 w-auto"
+            src="/img/auctions.svg"
+            alt="Auctions illustration"
+            width={134}
+            height={126}
+          />
+        }
+      >
+        <h1 className="text-2xl font-bold">Auctions (beta)</h1>
+        <p className="w-full max-w-lg text-pretty pt-1">
+          Dutch auctions to enable trades for accounts using the{" "}
+          <Link
+            className="font-semibold hover:border-b-[1.6px] hover:border-valence-black"
+            href="/rebalancer"
+          >
+            Rebalancer
+          </Link>
+          . Auctions begin at 12:00 UTC daily, and close after 24 hours.
+        </p>
+        <div className="hidden flex-row flex-wrap gap-4 pt-2 sm:flex">
+          {heroLinks.map((link) => (
+            <Link
+              key={`hero-link-${link.label}`}
+              target="_blank"
+              href={link.href}
+            >
+              <Button
+                className="flex w-fit flex-row justify-center gap-1"
+                variant="secondary"
+              >
+                {link.label}
+                <HiMiniArrowRight className="h-4 w-4" />
+              </Button>
+            </Link>
+          ))}
+        </div>
+      </ValenceProductBrand>
+      <div className=" flex flex-row flex-wrap gap-4 pt-2 sm:hidden">
         {heroLinks.map((link) => (
           <Link
             key={`hero-link-${link.label}`}
@@ -44,7 +63,7 @@ export function LiveAuctionsHero() {
           </Link>
         ))}
       </div>
-    </ValenceProductBrand>
+    </>
   );
 }
 const heroLinks: { href: string; label: string }[] = [
