67bffce1ea99236950087863c37c64cc66478ecc
Lena <elena.mikhaylova0@gmail.com>
2024-10-25 05:42:03 +0700

    rm edge config

--- apps/valence-app/src/server/actions/index.ts
+++ apps/valence-app/src/server/actions/index.ts
@@ -2,7 +2,6 @@ export * from "./fetch-rebalancer-account-config";
 export * from "./fetch-account-balances";
 export * from "./fetch-historical-prices";
 export * from "./fetch-historical-balances";
-export * from "./fetch-edge-config";
 export * from "./fetch-historical-targets";
 export * from "./fetch-supported-balances";
 export * from "./fetch-prices";
