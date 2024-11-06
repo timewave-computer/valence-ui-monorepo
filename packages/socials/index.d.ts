e1542bbf486f8c103336d0501a90f0f74f8df912
Lena <elena.mikhaylova0@gmail.com>
2024-11-06 11:47:44 +0700

    rm souremap files

--- packages/socials/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const X_URL = "https://x.com/ValenceZone";
-export declare const X_HANDLE = "@ValenceZone";
-export declare const VALENCE_DOMAIN = "valence.zone";
-export declare const GITHUB_URL = "https://github.com/timewave-computer";
-//# sourceMappingURL=index.d.ts.map
