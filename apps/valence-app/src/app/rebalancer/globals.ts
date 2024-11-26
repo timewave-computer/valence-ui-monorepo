cf7a1c1c920ea7ac039fa30007e2ed8d79a05b6e
Lena <elena.mikhaylova0@gmail.com>
2024-11-26 15:41:29 +0700

    fix build

--- apps/valence-app/src/app/rebalancer/globals.ts
+++ apps/valence-app/src/app/rebalancer/globals.ts
@@ -2,7 +2,7 @@ import { atom } from "jotai";
 import { Scale } from "@/app/rebalancer/const";
 import { chainConfig } from "@/const/config";
 
-export const accountAtom = atom();
+export const accountAtom = atom("");
 export const scaleAtom = atom<Scale>(Scale.Month);
 export const baseDenomAtom = atom<string>(chainConfig.defaultBaseTokenDenom);
 export const priceSourceAtom = atom<"coingecko" | "oracle">("coingecko");
