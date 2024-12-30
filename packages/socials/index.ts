90e9192e76b13d2e6b1eeecb6fee67acc446ff1e
Lena <elena.mikhaylova0@gmail.com>
2024-12-30 11:51:18 -0500

    add docs and update testnet indexer

--- packages/socials/index.ts
+++ packages/socials/index.ts
@@ -2,3 +2,4 @@ export const X_URL = "https://x.com/ValenceZone";
 export const X_HANDLE = "@ValenceZone";
 export const VALENCE_DOMAIN = "valence.zone";
 export const GITHUB_URL = "https://github.com/timewave-computer";
+export const DOCS_URL = "https://docs.valence.zone";
