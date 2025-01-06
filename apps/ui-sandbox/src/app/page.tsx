57a5c37eb25c9f0676e572d67d1fc9782fb59907
Lena <elena.mikhaylova0@gmail.com>
2025-01-06 12:28:11 -0500

    change require to dynamic import

--- apps/ui-sandbox/src/app/page.tsx
+++ apps/ui-sandbox/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { StoryLabel } from "~/components";
 import { getStories, GetStories } from "~/lib";
+import dynamic from "next/dynamic";
 
 export default function Home() {
   const stories = getStories();
@@ -8,11 +9,15 @@ export default function Home() {
       <StoryLabel className="text-h5">All</StoryLabel>
 
       {stories.map((story: GetStories[number]) => {
-        const StoryComponent = require(`./stories/${story.fileName}`).default;
+        const StoryComponent = dynamic(
+          () => import(`./stories/${story.fileName}`),
+          {
+            loading: () => <p>Loading...</p>,
+          }
+        );
         return (
           <div className="p-4 flex flex-col gap-2" key={`story-${story.id}`}>
             <StoryLabel>{story.prettyName}</StoryLabel>
-
             <StoryComponent key={story.id} />
           </div>
         );
