367314774ab72d4010ce9d9c660381eb7676c11a
Lena <elena.mikhaylova0@gmail.com>
2025-03-04 10:19:14 -0500

    reports

---
+++ apps/valence-app/src/app/internal/README.md
@@ -0,0 +1,5 @@
+# Reports
+
+Internal. Only works in dev.
+
+This is just some infrastructure for the team to easily get prices or other data if needed.
---
+++ apps/valence-app/src/app/internal/reports/historical-neutron-price/page.tsx
@@ -0,0 +1,30 @@
+import { fetchHistoricalPricesV2 } from "@/server/actions";
+import { UTCDate } from "@date-fns/utc";
+import { format } from "date-fns";
+
+/***
+ * this is only here for internal purposes
+ */
+export default async function Report() {
+  const historicalPrices = await fetchHistoricalPricesV2({
+    denom: "untrn",
+    coingeckoId: "neutron-3",
+  });
+
+  return (
+    <div>
+      {historicalPrices.map((row, i) => {
+        const date = new UTCDate(row[0]);
+        const formattedDate = format(date, "MM/dd/yyyy hh:mm:ss a");
+
+        return (
+          <div key={row[0]}>
+            <div>
+              {formattedDate},{row[1]}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
