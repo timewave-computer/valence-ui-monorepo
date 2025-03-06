f2da108f70e12ee6c4074c1dc3ad031010a797ea
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2025-03-06 09:04:38 -0500

    fix loader width (#72)

--- apps/valence-app/src/app/programs/ui/SuspenseLoadingSkeleton.tsx
+++ apps/valence-app/src/app/programs/ui/SuspenseLoadingSkeleton.tsx
@@ -5,7 +5,7 @@ export const SuspenseLoadingSkeleton = () => {
   return (
     <div className="p-4 grow flex flex-col items-start">
       <LoadingSkeleton className="h-[72px]  w-1/5 sm:w-1/3" />
-      <div className="flex w-full max-w-[1600px] grow flex-col pt-4">
+      <div className="flex w-full grow flex-col pt-4">
         <LoadingSkeleton className="h-full w-full grow" />
       </div>
     </div>
